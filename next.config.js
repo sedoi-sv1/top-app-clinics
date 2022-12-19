625773f9a4edd2d2b0a3e4d8e6458f8eb3d2d6f5
Sedoi <sedoi.sv1@yandex.ru>
2022-12-20 05:45:44 +0700

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   components/Button/Button.module.css 	new file:   components/Button/Button.props.ts 	new file:   components/Button/Button.tsx 	new file:   components/Button/arrow.svg 	new file:   components/Htag/Htag.module.css 	new file:   components/Htag/Htag.props.ts 	new file:   components/Htag/Htag.tsx 	new file:   components/index.ts 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/_app.tsx 	new file:   pages/_document.tsx 	new file:   pages/index.tsx 	new file:   public/favicon.ico 	new file:   public/vercel.svg 	new file:   styles/globals.css 	new file:   tsconfig.json

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	/*images: {
+		domains: ['courses-top.ru']
+	},*/
+    reactStrictMode: true,
+    webpack(config){
+    config.module.rules.push({
+        test: /\.svg$/,
+        issuer: /\.(js|ts)x?$/,
+        use: ['@svgr/webpack'],
+    });
+    return config;
+    },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Htag, Button } from '../components';
+
+
+export default function Home(): JSX.Element {
+	return (
+		<div>
+			<Htag tag='h1'>Текст</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+		</div>
+	);
+}
