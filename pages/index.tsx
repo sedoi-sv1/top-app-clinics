3d83caecc43f67e789520d2e39787b4b8d1e2c3a
Sedoi <sedoi.sv1@yandex.ru>
2022-12-20 06:30:02 +0700

    VS code add P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Htag, Button } from '../components';
+import { Htag, Button, P } from '../components';
 
 
 export default function Home(): JSX.Element {
@@ -7,7 +7,17 @@ export default function Home(): JSX.Element {
 		<div>
 			<Htag tag='h1'>Текст</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
-			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<Button appearance='ghost' arrow='right'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			{/*<Tag size='s'>Ghost</Tag>
+			<Tag size='s' color='red'>Red</Tag>
+			<Tag size='m' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+			<Input placeholder='Тест' />
+	<TextArea placeholder='Тест area' />*/}
 		</div>
 	);
 }
