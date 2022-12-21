a9af7064a2020b5285df11a318541df344e70eac
Sedoi <sedoi.sv1@yandex.ru>
2022-12-21 13:16:23 +0700

    VS code add Rating

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/jsx-key */
+import { RatingProps } from './Ratjng.props';
+import styles from './Rating.module.css';
+import StarIcon from './star.svg';
+import cn from 'classnames';
+import { useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef } from 'react';
+
+export const Rating = forwardRef(({ isEditable = false, rating, setRating, error, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+
+	useEffect(() => {
+		constructRating(rating);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [rating]);
+
+	const constructRating = (curentRating: number) => {
+		const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
+			return (
+				<span>
+					<StarIcon
+						className={cn(styles.star, {
+							[styles.filled]: i < curentRating,
+							[styles.editable]: isEditable
+						})}
+
+						onMouseEnter={() => changeDispay(i + 1)}
+						onMouseLeave={() => changeDispay(rating)}
+						onClick={() => onClick(i + 1)}
+						tabIndex={isEditable ? 0 : -1}
+						onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
+
+					/>
+				</span>
+			);
+		});
+		setRatingArray(updatedArray);
+	};
+
+	const changeDispay = (i: number) => {
+		if (!isEditable) {
+			return;
+		}
+		constructRating(i);
+	}
+
+	const onClick = (i: number) => {
+		if (!isEditable || !setRating) {
+			return;
+		}
+		setRating(i);
+	}
+
+	const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+		if (e.code != 'Spase' || !setRating) {
+			return;
+		}
+		setRating(i);
+	}
+
+	return (
+		<div {...props} ref={ref} className={cn(styles.ratingWrapper, {
+			[styles.error]: error
+		})}>
+			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
+		</div>
+	)
+
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Htag, Button, P } from '../components';
+import React, { useState } from 'react';
+import { Htag, Button, P, Tag, Rating } from '../components';
 
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 	return (
 		<div>
 			<Htag tag='h1'>Текст</Htag>
@@ -11,12 +12,12 @@ export default function Home(): JSX.Element {
 			<P size='l'>Большой</P>
 			<P>Средний</P>
 			<P size='s'>Маленький</P>
-			{/*<Tag size='s'>Ghost</Tag>
+			<Tag size='s'>Ghost</Tag>
 			<Tag size='s' color='red'>Red</Tag>
 			<Tag size='m' color='green'>Green</Tag>
 			<Tag color='primary'>Green</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-			<Input placeholder='Тест' />
+			{/*<Input placeholder='Тест' />
 	<TextArea placeholder='Тест area' />*/}
 		</div>
 	);
